d699220989bf1b6efcdf19a5280f5c7a44b76730
JoaoPe22 <joao.canezin22@gmail.com>
2024-05-24 16:56:02 -0400

    commit

---
+++ shop-main/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"YCCUeIdKJS6NPCayR+adLuGfbDDhY0GWrJ0azQLiA34=\"\n}"
--- shop-main/src/app/components/Main.js
+++ shop-main/src/app/components/Main.js
@@ -1,16 +1,20 @@
-export default function Main(){
+
+
+export default async function Main(){
     const response = await fetch("https://api.escuelajs.co/api/v1/products");
     const data = await response.json();
   
     return(
+      <main>
         {data.map((products) => (
             <div key={products.id}>
               <h1>{products.title}</h1>
               <h2>{products.price}</h2>
               <p>{products.description}</p>
-              <p>{products.category}</p>
-              <image src={products.images} width={100} height={100} alt='imagem de produto'></image>
+              {/* <p>{products.category.name}</p>
+              <img src={products.images} width={100} height={100} alt='imagem de produto'></img> */}
             </div>
           ))}
+        </main>
     )
-}
+}  
