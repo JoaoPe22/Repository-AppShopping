888cc238f919faa1b9594a03437617c28dc6fb49
Igomesxr2 <joao.canezin22@gmail.com>
2024-07-03 08:29:46 -0400

    commit

--- shop-main/.next/server/server-reference-manifest.js
+++ shop-main/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"4dFyRU70vXbOVY8fhGRbGpDiUvbOmkDyu0QqP82PccA=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"2dZ4tFsLjP/PrIoMQLiD12PfPDqqt5QN2Blp+NJyKMo=\"\n}"
