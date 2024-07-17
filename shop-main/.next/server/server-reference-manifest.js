42df8c8194a536d63d3c7912ab94a6fd1ca1b5f2
JoaoPe22 <joao.canezin22@gmail.com>
2024-07-17 12:27:25 -0400

    commit

--- shop-main/.next/server/server-reference-manifest.js
+++ shop-main/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"2dZ4tFsLjP/PrIoMQLiD12PfPDqqt5QN2Blp+NJyKMo=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"g2fk2qXt5Parbsy0ElLrJLLFF9bpm/8IXN1X39aKNZU=\"\n}"
